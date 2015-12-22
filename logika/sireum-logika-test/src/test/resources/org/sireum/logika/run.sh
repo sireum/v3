b4738fabd805344f4d74c1c735f6e82b41cc520d
Robby <robby@k-state.edu>
2015-12-21 19:03:11 -0600

    Use Scala fsc but reset it before running examples.

--- logika/sireum-logika-test/src/test/resources/org/sireum/logika/run.sh
+++ logika/sireum-logika-test/src/test/resources/org/sireum/logika/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Make sure to create Sireum fat jar first using Sbt before running this script."
+fsc -reset
 for i in *.scala; do
-    scala -nc -classpath ../../../../../../../../jvm/target/scala-2.11/sireum.jar $i
+    scala -classpath ../../../../../../../../jvm/target/scala-2.11/sireum.jar $i
 done
