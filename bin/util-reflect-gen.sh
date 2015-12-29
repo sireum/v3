7111bbdf830a2fe017750403227f5da89d90589d
Robby <robby@k-state.edu>
2015-12-29 11:59:15 -0600

    Revert previous change that uses Sireum script due to local changes causing Sireum jar rebuilding.

--- bin/util-reflect-gen.sh
+++ bin/util-reflect-gen.sh
@@ -25,10 +25,10 @@
 #
 SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 /bin/bash ${SCRIPT_HOME}/sbt-launch.sh assembly
-SIREUM=${SCRIPT_HOME}/../sireum
-/bin/bash ${SIREUM} util reflect rewriter -d core/sireum-pilar/src/main/scala -c org.sireum.pilar.ast.Rewriter -l license.txt org.sireum.pilar.ast.Node
-/bin/bash ${SIREUM} util reflect json -d core/sireum-pilar/src/main/scala -c org.sireum.pilar.ast.Json -l license.txt org.sireum.pilar.ast.Node
-/bin/bash ${SIREUM} util reflect cli -c org.sireum.Cli -d cli/sireum-cli/src/main/scala -l license.txt org.sireum.option.SireumOption
-/bin/bash ${SIREUM} util reflect json -d java/sireum-java/src/main/scala -c org.sireum.java.meta.JavaMetaJson -l license.txt org.sireum.java.meta.JavaMeta
-/bin/bash ${SIREUM} util reflect rewriter -d logika/sireum-logika/src/main/scala -c org.sireum.logika.ast.Rewriter -l license.txt org.sireum.logika.ast.Node
+SIREUM="${SCRIPT_HOME}/../platform/java/bin/java -jar ${SCRIPT_HOME}/../jvm/target/scala-2.11/sireum.jar"
+${SIREUM} util reflect rewriter -d core/sireum-pilar/src/main/scala -c org.sireum.pilar.ast.Rewriter -l license.txt org.sireum.pilar.ast.Node
+${SIREUM} util reflect json -d core/sireum-pilar/src/main/scala -c org.sireum.pilar.ast.Json -l license.txt org.sireum.pilar.ast.Node
+${SIREUM} util reflect cli -c org.sireum.Cli -d cli/sireum-cli/src/main/scala -l license.txt org.sireum.option.SireumOption
+${SIREUM} util reflect json -d java/sireum-java/src/main/scala -c org.sireum.java.meta.JavaMetaJson -l license.txt org.sireum.java.meta.JavaMeta
+${SIREUM} util reflect rewriter -d logika/sireum-logika/src/main/scala -c org.sireum.logika.ast.Rewriter -l license.txt org.sireum.logika.ast.Node
 /bin/bash ${SCRIPT_HOME}/sbt-launch.sh assembly
