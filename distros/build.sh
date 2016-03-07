cda99b132572d7475b6011c7e29d9ca0b0ccc6d4
Robby <robby@k-state.edu>
2016-03-06 19:51:09 -0600

    Added distro build script.

---
+++ distros/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+if [ ! -d sireum-v3 ]; then
+  git clone https://github.com/sireum/v3.git sireum-v3
+fi
+cd sireum-v3
+git pull --all
+git checkout master
+rm -fR distros
+export SIREUM_PARALLEL_BUILD=false
+bin/sbt-launch.sh clean test:compile test
+./sireum
+bin/sbt-launch.sh distros
