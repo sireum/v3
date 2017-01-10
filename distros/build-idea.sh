2a701169f65b60d4924a9c15da0eab0acd3ad0e2
Robby <robby@k-state.edu>
2017-01-09 20:37:21 -0600

    Removed requirement for full clone for the scripts to run.

--- distros/build-idea.sh
+++ distros/build-idea.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 export SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
+export SIREUM_IDEA_DISTROS=true
 $SCRIPT_DIR/build.sh
-$SCRIPT_DIR/../bin/sbt-launch.sh idea-distros
+$SCRIPT_DIR/sireum-v3/bin/sbt-launch.sh idea-distros
+mv $SCRIPT_DIR/sireum-v3/distros/*.zip $SCRIPT_DIR/
+mv $SCRIPT_DIR/sireum-v3/distros/*.tar.gz $SCRIPT_DIR/
+mv $SCRIPT_DIR/sireum-v3/distros/*.dmg $SCRIPT_DIR/
+mv $SCRIPT_DIR/sireum-v3/distros/sireum-v3-VER $SCRIPT_DIR/
+mv $SCRIPT_DIR/sireum-v3/distros/idea $SCRIPT_DIR/
+mv $SCRIPT_DIR/sireum-v3/resources/distro/resource_hacker $SCRIPT_DIR/
+rm -fR sireum-v3
--- distros/build.sh
+++ distros/build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash -e
 export SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-rm -fR *.zip sireum-v3-VER sireum-v3
+rm -fR sireum-v3-mac64.zip sireum-v3-linux64.zip sireum-v3-win64.zip sireum-v3-VER sireum-v3
 export SIREUM_SKIP_BUILD=false
 git clone -b master https://github.com/sireum/v3.git sireum-v3
+if [ -d idea ] && [ -d resource_hacker ] && [ "$SIREUM_IDEA_DISTROS" == "true" ]; then
+  mv idea sireum-v3/distros
+  mv resource_hacker sireum-v3/resources/distro
+fi
 cd sireum-v3
 export SIREUM_PARALLEL_BUILD=false
 bin/sbt-launch.sh clean test:compile test
 ./sireum
 bin/sbt-launch.sh distros
-mv $SCRIPT_DIR/sireum-v3/distros/*.zip $SCRIPT_DIR/
-mv $SCRIPT_DIR/sireum-v3/distros/sireum-v3-VER $SCRIPT_DIR/
-rm -fR $SCRIPT_DIR/sireum-v3
+if [ "$SIREUM_IDEA_DISTROS" != "true" ]; then
+  mv $SCRIPT_DIR/sireum-v3/distros/*.zip $SCRIPT_DIR/
+  mv $SCRIPT_DIR/sireum-v3/distros/sireum-v3-VER $SCRIPT_DIR/
+  rm -fR sireum-v3
+fi
