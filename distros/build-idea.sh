a1a16eda69fb7f0a4d0e4691ee4f6c56a21ab362
Robby <robby@k-state.edu>
2017-01-10 08:51:59 -0600

    Updated.

--- distros/build-idea.sh
+++ distros/build-idea.sh
@@ -3,6 +3,9 @@ export SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 export SIREUM_IDEA_DISTROS=true
 rm -f sireum-v3-idea*
 $SCRIPT_DIR/build.sh
+if [ -e idea.exe ] && [ -e idea64.exe ]; then
+  cp idea.exe idea64.exe sireum-v3/distros
+fi
 $SCRIPT_DIR/sireum-v3/bin/sbt-launch.sh idea-distros
 mv $SCRIPT_DIR/sireum-v3/distros/*.zip $SCRIPT_DIR/
 mv $SCRIPT_DIR/sireum-v3/distros/*.tar.gz $SCRIPT_DIR/
