e5f36892f1b55361da93283635bf1545bd257bc7
Robby <robby@k-state.edu>
2017-01-20 19:00:49 -0600

    Use symlink for cached resources.

--- distros/build-ive.sh
+++ distros/build-ive.sh
@@ -18,6 +18,10 @@ mv $SCRIPT_DIR/sireum-v3/distros/*.zip $SCRIPT_DIR/
 mv $SCRIPT_DIR/sireum-v3/distros/*.tar.gz $SCRIPT_DIR/
 mv $SCRIPT_DIR/sireum-v3/distros/*.dmg $SCRIPT_DIR/
 mv $SCRIPT_DIR/sireum-v3/distros/sireum-v3*-VER $SCRIPT_DIR/
-mv $SCRIPT_DIR/sireum-v3/distros/idea $SCRIPT_DIR/
-mv $SCRIPT_DIR/sireum-v3/resources/distro/resource_hacker $SCRIPT_DIR/
+if [ ! -d $SCRIPT_DIR/idea ]; then
+  mv $SCRIPT_DIR/sireum-v3/distros/idea $SCRIPT_DIR/
+fi
+if [ ! -d $SCRIPT_DIR/resource_hacker ]; then
+  mv $SCRIPT_DIR/sireum-v3/resources/distro/resource_hacker $SCRIPT_DIR/
+fi
 rm -fR sireum-v3 sireum-v3-dev
