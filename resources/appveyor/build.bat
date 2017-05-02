6b525a22a3a8d71efc1e5590d4ef5efe76d6bd33
Robby <robby@k-state.edu>
2017-05-02 12:03:29 -0500

    Updated.

--- resources/appveyor/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-IF %COMPILER%==msys2 (
-  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-  bash -lc "$APPVEYOR_BUILD_FOLDER/bin/sbt-launch.sh clean test:compile"
-)
--- resources/appveyor/install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-IF %COMPILER%==msys2 (
-  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
-  bash -lc "pacman --noconfirm -S git unzip wget"
-  bash -lc "$APPVEYOR_BUILD_FOLDER/bin/prelude.sh"
-)
