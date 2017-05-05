214191c6cf7f9a026e930e4b34395416d845a058
Robby <robby@k-state.edu>
2017-05-05 09:15:03 -0500

    Further timeout tweaks for macOS build.

--- logika/jvm/src/test/scala/org/sireum/logika/test/SymExeProgramTestDefProvider.scala
+++ logika/jvm/src/test/scala/org/sireum/logika/test/SymExeProgramTestDefProvider.scala
@@ -62,10 +62,10 @@ final class SymExeProgramTestDefProvider(tf: TestFramework)
       ConditionTest("symexe/pure-invoke", check("symexe/pure-invoke", 0, isSummarizing = true)) :+
       ConditionTest("symexe/pure-invoke2", check("symexe/pure-invoke2", 0, isSummarizing = true)) :+
       ConditionTest("usymexe/abs-top", check("symexe/abs-top", 8, isSummarizing = false)) :+
-      ConditionTest("usymexe/ffsS8-top", check("symexe/ffsS8-top", 8, isSummarizing = false, timeout = 5000 * tmMult)) :+
-      ConditionTest("usymexe/ffsU32-top", check("symexe/ffsU32-top", 8, isSummarizing = false, timeout = 5000 * tmMult)) :+
-      ConditionTest("usymexe/ffsS8-loop", check("symexe/ffsS8-loop", 8, isSummarizing = false, timeout = 5000 * tmMult)) :+
-      ConditionTest("usymexe/ffsU32-loop", check("symexe/ffsU32-loop", 8, isSummarizing = false, timeout = 5000 * tmMult)) :+
+      ConditionTest("usymexe/ffsS8-top", check("symexe/ffsS8-top", 8, isSummarizing = false, timeout = 8000 * tmMult)) :+
+      ConditionTest("usymexe/ffsU32-top", check("symexe/ffsU32-top", 8, isSummarizing = false, timeout = 8000 * tmMult)) :+
+      ConditionTest("usymexe/ffsS8-loop", check("symexe/ffsS8-loop", 8, isSummarizing = false, timeout = 8000 * tmMult)) :+
+      ConditionTest("usymexe/ffsU32-loop", check("symexe/ffsU32-loop", 8, isSummarizing = false, timeout = 8000 * tmMult)) :+
       ConditionTest("usymexe/ffsU32-loop-bug", check("symexe/ffsU32-loop-bug", 8, isSummarizing = false, hasError = true))
       ) ++
       (1 to 14).toVector.map { x =>
