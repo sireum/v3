25d3e6fa90a4057bb38e40632c643acf916d18d9
Robby <robby@k-state.edu>
2017-07-05 20:58:54 -0500

    Added.

---
+++ slang/shared/src/main/scala/org/sireum/lang/symbol/Resolver.scala
@@ -0,0 +1,87 @@
+// #Sireum
+/*
+ Copyright (c) 2017, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sireum.lang.symbol
+
+import org.sireum._
+import org.sireum.lang.util.Reporter
+import org.sireum.lang.{ast => AST}
+
+object Resolver {
+
+  @datatype trait Info
+
+  @datatype class PackageInfo(ids: ISZ[String])
+    extends Info
+
+  val rootPackageInfo: PackageInfo = PackageInfo(ISZ())
+}
+
+import Resolver._
+
+@record class GlobalDeclarationResolver(var globalNameMap: Map[ISZ[String], Resolver.Info],
+                                        var packageInfo: PackageInfo,
+                                        reporter: Reporter) {
+
+  def resolveProgram(program: AST.TopUnit.Program): AST.TopUnit.Program = {
+    declarePackageName(program.packageName) match {
+      case Some(info) => packageInfo = info
+      case _ => return program
+    }
+
+    ???
+  }
+
+  def declarePackage(ids: ISZ[AST.Id]): Unit = {
+    val name = AST.Util.ids2strings(ids)
+    globalNameMap.get(name) match {
+      case Some(info: PackageInfo) =>
+      case Some(_) =>
+        val posOpt = ids(ids.size - z"1").attr.posInfoOpt
+        reporter.error(posOpt, s"Cannot declare package name because the name has already been used for a non-package.")
+      case _ => globalNameMap = globalNameMap.put(name, PackageInfo(name))
+    }
+  }
+
+  def declarePackageName(name: AST.Name): Option[PackageInfo] = {
+    val ids = name.ids
+    if (name.ids.isEmpty) {
+      return Some(rootPackageInfo)
+    }
+    var currentIds = ISZ(ids(z"0"))
+    declarePackage(currentIds)
+
+    for (i <- 1 until ids.size) {
+      currentIds = currentIds :+ ids(i)
+      declarePackage(currentIds)
+    }
+
+    globalNameMap.get(AST.Util.ids2strings(currentIds)) match {
+      case Some(info: PackageInfo) => return Some(info)
+      case _ => return None()
+    }
+  }
+}
