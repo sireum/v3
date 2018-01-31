6ba853cad3a795ab87f8edd63b2a7f8a89a905ac
Robby <robby@k-state.edu>
2018-01-31 08:46:35 -0600

    Revised to use SIREUM_HOME, if available.

--- slang/shared/src/main/scala/org/sireum/cli/regen.sh
+++ slang/shared/src/main/scala/org/sireum/cli/regen.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-CLI=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-ROOT=$CLI/../../../../../../../..
-JAVA=$ROOT/platform/java/bin/java
-$JAVA -cp $ROOT/bin/sireum.jar org.sireum.cli.Sireum util cligen -l $ROOT/license.txt -p org.sireum.cli -w 25,55 CliConfig.sc
+: ${SIREUM_HOME:=$( cd "$( dirname "$0" )"/../../../../../../../.. &> /dev/null && pwd )}
+$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util cligen -l $SIREUM_HOME/license.txt -p org.sireum.cli -w 25,55 CliConfig.sc
--- slang/shared/src/main/scala/org/sireum/lang/ast/regen.sh
+++ slang/shared/src/main/scala/org/sireum/lang/ast/regen.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-AST=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
-ROOT=$AST/../../../../../../../../..
-JAVA=$ROOT/platform/java/bin/java
-$JAVA -cp $ROOT/bin/sireum.jar org.sireum.cli.Sireum util sergen -l $ROOT/license.txt -m json,msgpack AST.scala
-$JAVA -cp $ROOT/bin/sireum.jar org.sireum.cli.Sireum util transgen -l $ROOT/license.txt -m immutable,mutable AST.scala
+: ${SIREUM_HOME:=$( cd "$( dirname "$0" )"/../../../../../../../../.. &> /dev/null && pwd )}
+$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util sergen -l $SIREUM_HOME/license.txt -m json,msgpack AST.scala
+$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util transgen -l $SIREUM_HOME/license.txt -m immutable,mutable AST.scala
