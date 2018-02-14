a7bfd99c43f267ccee372ceee6c888a919d23ce3
Robby <robby@k-state.edu>
2018-02-13 20:06:44 -0600

    Updated CLI and Slang AST regen scripts.

--- slang/shared/src/main/scala/org/sireum/cli/regen.sh
+++ slang/shared/src/main/scala/org/sireum/cli/regen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 : ${SIREUM_HOME:=$( cd "$( dirname "$0" )"/../../../../../../../.. &> /dev/null && pwd )}
-$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util cligen -l $SIREUM_HOME/license.txt -p org.sireum.cli -w 25,55 CliConfig.sc
+$SIREUM_HOME/platform/java/bin/java -jar $SIREUM_HOME/bin/sireum.jar util cligen -l $SIREUM_HOME/license.txt -p org.sireum.cli -w 25,55 CliConfig.sc
--- slang/shared/src/main/scala/org/sireum/lang/ast/regen.sh
+++ slang/shared/src/main/scala/org/sireum/lang/ast/regen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 : ${SIREUM_HOME:=$( cd "$( dirname "$0" )"/../../../../../../../../.. &> /dev/null && pwd )}
-$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util sergen -l $SIREUM_HOME/license.txt -m json,msgpack AST.scala
-$SIREUM_HOME/platform/java/bin/java -cp $SIREUM_HOME/bin/sireum.jar org.sireum.cli.Sireum util transgen -l $SIREUM_HOME/license.txt -m immutable,mutable AST.scala
+$SIREUM_HOME/platform/java/bin/java -jar $SIREUM_HOME/bin/sireum.jar util sergen -l $SIREUM_HOME/license.txt -m json,msgpack AST.scala
+$SIREUM_HOME/platform/java/bin/java -jar $SIREUM_HOME/bin/sireum.jar util transgen -l $SIREUM_HOME/license.txt -m immutable,mutable AST.scala
