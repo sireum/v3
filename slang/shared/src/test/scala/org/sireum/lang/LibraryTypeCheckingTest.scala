fe806ce9491c5a5c4a11c4cd55dbdb4526526c65
Robby <robby@k-state.edu>
2018-02-14 20:11:58 -0600

    Added type checker test for Slang runtime library.

--- slang/shared/src/test/scala/org/sireum/lang/LibraryTypeCheckingTest.scala
+++ slang/shared/src/test/scala/org/sireum/lang/LibraryTypeCheckingTest.scala
@@ -1,5 +1,66 @@
+/*
+ Copyright (c) 2018, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.sireum.lang
 
-class LibraryTypeCheckingTest {
+import org.sireum._
+import org.sireum.lang.symbol._
+import org.sireum.lang.tipe._
+import org.sireum.lang.util.AccumulatingReporter
+import org.sireum.test.SireumSpec
 
+class LibraryTypeCheckingTest extends SireumSpec {
+
+  {
+
+    implicit val _spec: SireumSpec = this
+
+    * {
+      val (tc, rep) = TypeChecker.libraryCheckerReporter
+      def nameInfo(name: Predef.String): (ISZ[String], Resolver.Info) = {
+        val ids = ISZ(name.split('.').map(s => s: String): _*)
+        return (ids, tc.typeHierarchy.nameMap.get(ids).get)
+      }
+      def typeInfo(name: Predef.String): (ISZ[String], Resolver.TypeInfo) = {
+        val ids = ISZ(name.split('.').map(s => s: String): _*)
+        return (ids, tc.typeHierarchy.typeMap.get(ids).get)
+      }
+      val reporter = AccumulatingReporter.create
+      reporter.reports(rep.messages)
+      val all = F
+      val name = "org.sireum.Map"
+      val nameMap: Resolver.NameMap = if (all) tc.typeHierarchy.nameMap else HashMap ++ ISZ(
+        nameInfo(name)
+      )
+      val typeMap: Resolver.TypeMap = if (all) tc.typeHierarchy.typeMap else HashMap ++ ISZ(
+        typeInfo(name)
+      )
+      TypeChecker.checkComponents(tc.typeHierarchy, nameMap, typeMap, reporter)
+      reporter.printMessages()
+      !reporter.hasIssue
+    }
+
+  }
 }
