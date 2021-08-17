d292236895d6cdbec861a9ccaf7015c99235e485
Robby <robby@santoslab.org>
2021-08-17 16:32:35 -0500

    Upgraded z3.

--- bin/prelude.sh
+++ bin/prelude.sh
@@ -31,7 +31,7 @@ ZULU_VERSION=16.32.15-ca-jdk16.0.2
 SCALA_VERSION=2.13.6
 SBT_VERSION=1.5.5
 NODE_VERSION=14.17.5
-Z3_VERSION=4.8.9
+Z3_VERSION=4.8.12
 if [ -z "${PLATFORM}" ]; then
   if [ -n "$COMSPEC" -a -x "$COMSPEC" ]; then
     PLATFORM=win
@@ -48,12 +48,12 @@ if [ "${PLATFORM}" = "win"  ]; then
 elif [ "${PLATFORM}" = "mac"  ]; then
   ZULU_DROP_URL=https://cdn.azul.com/zulu/bin/zulu${ZULU_VERSION}-macosx_x64.tar.gz
   NODE_DROP_URL=https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-darwin-x64.tar.gz
-  Z3_DROP_URL=https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-x64-osx-10.14.6.zip
+  Z3_DROP_URL=https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-x64-osx-10.15.7.zip
 elif [ "${PLATFORM}" = "linux"  ]; then
   type -P xz &>/dev/null || { >&2 echo "xz command not found."; exit 1; }
   ZULU_DROP_URL=https://cdn.azul.com/zulu/bin/zulu${ZULU_VERSION}-linux_x64.tar.gz
   NODE_DROP_URL=https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
-  Z3_DROP_URL=https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-x64-ubuntu-16.04.zip
+  Z3_DROP_URL=https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-x64-glibc-2.31.zip
 else
   >&2 echo "Sireum does not support: $(uname)."
   exit 1
